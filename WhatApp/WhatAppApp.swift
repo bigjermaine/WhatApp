c02ee182805b078f9691704fcee036784ebc09b6
BigJermaine <113020989+bigjermaine@users.noreply.github.com>
2024-05-22 09:58:54 +0100

    Initial Commit

---
+++ WhatApp/WhatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatAppApp.swift
+//  WhatApp
+//
+//  Created by MacBook AIR on 22/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct WhatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
