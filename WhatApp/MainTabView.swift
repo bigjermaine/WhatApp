539fc60199dd9f446e910297f2edfbfcce08d58c
BigJermaine <113020989+bigjermaine@users.noreply.github.com>
2024-05-22 10:34:24 +0100

    created tabView File

---
+++ WhatApp/MainTabView.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabView.swift
+//  WhatApp
+//
+//  Created by MacBook AIR on 22/05/2024.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView{
+            
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+}
