6ba959cf9a54b2b8cfe2269384133182d1f40fc8
BigJermaine <113020989+bigjermaine@users.noreply.github.com>
2024-05-23 09:04:13 +0100

    finished up the tab items

--- WhatApp/MainTabView.swift
+++ WhatApp/MainTabView.swift
@@ -10,11 +10,62 @@ import SwiftUI
 struct MainTabView: View {
     var body: some View {
         TabView{
-            
+            Text("Updates")
+            .tabItem {
+                Image(systemName: Tab.update.icon)
+                Text(Tab.update.title)
+                
+            }
+            Text("Update")
+            .tabItem {
+                Image(systemName: Tab.calls.icon)
+                Text(Tab.calls.title)
+                
+            }
+            Text("Updates")
+            .tabItem {
+                Image(systemName: Tab.communities.icon)
+                Text(Tab.communities.title)
+                
+            }
+            Text("Updates")
+            .tabItem {
+                Image(systemName: Tab.chats.icon)
+                Text(Tab.chats.title)
+                
+            }
+            Text("Updates")
+            .tabItem {
+                Image(systemName: Tab.settings.icon)
+                Text(Tab.settings.title)
+                
+            }
         }
     }
 }
 
+extension MainTabView {
+    private enum Tab:String{
+        case update, calls, communities, chats,settings
+        fileprivate var title:String{
+            return rawValue.capitalized
+        }
+        fileprivate var icon:String {
+            switch self {
+            case .update:
+                return "circle.dashed.inset.filled"
+            case .calls:
+                return "phone"
+            case .communities:
+                return "person.3"
+            case .chats:
+                return "message"
+            case .settings:
+                return "gear"
+            }
+        }
+    }
+}
 #Preview {
     MainTabView()
 }
