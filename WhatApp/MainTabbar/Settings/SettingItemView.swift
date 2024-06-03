63f0f2117388504647a04be50d817eca5044ff54
BigJermaine <113020989+bigjermaine@users.noreply.github.com>
2024-06-03 15:21:11 +0100

    started the settingsView

---
+++ WhatApp/MainTabbar/Settings/SettingItemView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingItemView.swift
+//  WhatApp
+//
+//  Created by MacBook AIR on 03/06/2024.
+//
+
+import SwiftUI
+
+struct SettingItemView: View {
+    var body: some View {
+        HStack{
+            
+        }
+    }
+}
+
+#Preview {
+    SettingItemView()
+}
