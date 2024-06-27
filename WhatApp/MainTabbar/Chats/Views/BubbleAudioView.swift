c42eedc2ea6aa5a064ec6bb59a32073d3f5a77d4
BigJermaine <113020989+bigjermaine@users.noreply.github.com>
2024-06-27 16:44:06 +0100

    created the buble audio view

---
+++ WhatApp/MainTabbar/Chats/Views/BubbleAudioView.swift
@@ -0,0 +1,18 @@
+//
+//  BubbleAudioView.swift
+//  WhatApp
+//
+//  Created by MacBook AIR on 27/06/2024.
+//
+
+import SwiftUI
+
+struct BubbleAudioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BubbleAudioView()
+}
